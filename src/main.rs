6446c7963fe213c49d318781f1a526f7d78d5471
luexp <10457192@qq.com>
2024-04-12 15:15:35 +0800

    raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+use std::io::prelude::*;
+use std::io::{Read, stdout, Write};
+use termion::raw::IntoRawMode;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    let mut stdin = stdin.lock();
+
+    stdout.flush().unwrap();
+
+    let mut c = [0;1];
+    loop {
+        io::
+        stdin.read(&mut c).unwrap();
+        if c[0] == 'q' as u8 {
+            break;
+        }
+        if c[0].is_ascii_control() {
+            print!("{}\r\n", c[0])
+        } else {
+            print!("{} ({})\r\n", c[0], c[0] as char)
+        }
+    }
+
 }
